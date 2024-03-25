ebf4412bd63675203c615df911be49c40b545d97
Xyberonzyx7 <advance.houdavid@outlook.com>
2024-03-25 09:41:51 -0700

    [add][queue] add queue algorithm

---
+++ lib/datastructure/CircularQueue.java
@@ -0,0 +1,57 @@
+package lib.datastructure;
+
+public class CircularQueue {
+	int N;
+	int[] storage;
+	int sz;
+	int front;
+	int rear;
+	
+	public CircularQueue(int N){
+		this.N = N;
+		storage = new int[N];
+		sz = 0;
+		front = rear = 0;
+	}
+
+	public void enqueue(int num){
+		if(sz == N){
+			return;
+		}else{
+			rear = (front + sz) % N;
+			storage[rear] = num;
+			sz = sz + 1;
+		}
+	}
+
+	public int dequeue(){
+		if(sz == 0){
+			return 0;
+		}else{
+			int num = storage[front];
+			front = (front + 1) % N;
+			sz = sz-1;
+			return num;
+		}
+	}
+
+	public int peekRear(){
+		return storage[rear];
+	}
+
+	public int peekFront(){
+		return storage[front];
+	}
+
+	public int rearIndex(){
+		return rear;
+	}
+
+	public int frontIndex(){
+		return front;
+	}
+
+	public int size(){
+		return sz;
+	}
+}
